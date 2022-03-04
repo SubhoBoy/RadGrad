51b774202fa127e658a951b587d20e1b93b56665
Subhodeep Chakraborty <subho07t@gmail.com>
2022-03-04 23:44:38 +0530

    Add logging

--- bot.py
+++ bot.py
@@ -1,12 +1,22 @@
 import json
+import logging
 
 import discord
 
+# get config values from config file
 with open("config.json") as f:
     config = json.load(f)
 
 token = config.get("token")
 
+# set up logging
+logger = logging.getLogger("discord")
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(handler)
 
 client = discord.Client()
 
