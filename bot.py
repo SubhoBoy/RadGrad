9e3357d1563db80f71aabd5c58e5dcf78c1077b6
Subhodeep Chakraborty <subho07t@gmail.com>
2022-03-11 23:40:32 +0530

    Improve config grabbing, bot run mode Read and write from and to json file Add function to automatically proceed as bot if there is no input

--- bot.py
+++ bot.py
@@ -3,25 +3,35 @@ from typing import Any
 from discord.ext import commands
 import json
 import logging
-
-try:
-    # get config values from config file
-    with open("config.json") as f:
-        config: dict = json.load(f)
-    token: str = str(config.get("token"))
-    user_token: str = str(config.get("user_token"))
-except FileNotFoundError:
-    token = os.environ["token"]
-
+from inputimeout import inputimeout, TimeoutOccurred
 
 # set up logging
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename="logs/new.log", encoding="utf-8", mode="w")
+handler = logging.FileHandler(filename="logs/bot.log", encoding="utf-8", mode="w")
 handler.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
 )
 logger.addHandler(handler)
+
+# get config values from config file
+try:
+    with open("config.json") as f:
+        config: dict = json.load(f)
+    logging.info("Config file found.")
+    token: str = str(config.get("token"))
+    if token is None:
+        logger.error("token not found in config file.")
+        print("token not found in config file.")
+        token = input("Please enter your token: ")
+        with open("config.json", "a") as f:
+            json.dump({"token": token}, f)
+    user_token: str = str(config.get("user_token"))
+except FileNotFoundError:
+    logger.warning(
+        "Config file not found. Attempting to get token from environment variables."
+    )
+    token = os.environ["token"]
 
 
 bot = commands.Bot(command_prefix="s!")
@@ -71,10 +81,22 @@ async def test(ctx):
     await ctx.send("Testing 1 2 3")
 
 
-mode = (input("Run as bot (b) or run as user (u)? ")).lower()
-if mode == "b":
-    bot.run(token)
-elif mode == "u":
-    bot.run(user_token, bot=False)
-else:
-    print("Invalid mode")
+def run_bot(token, user_token=None):
+    try:
+        mode = inputimeout(
+            prompt="Run as bot (b) or user (u)? (You have 1 second to answer)",
+            timeout=1,
+        )
+    except TimeoutOccurred:
+        mode = "b"
+        print("Proceeding as bot")
+        logging.info("Proceeding as bot as no input was given.")
+    if mode == "b":
+        bot.run(token)
+    elif mode == "u":
+        bot.run(user_token, bot=False)
+    else:
+        print("Invalid mode")
+
+
+run_bot(token, user_token, bot)
